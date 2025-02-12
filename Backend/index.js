87d7e846c8f640943ae48d44161a0ed7b33c11ff
Satyakarthik05 <160334326+Satyakarthik05@users.noreply.github.com>
2025-02-12 18:58:22 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -20,11 +20,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:8081",
-    "exp://192.168.208.105:8081",
-  ],
+  origin: "*",
   methods: ["POST", "GET", "DELETE", "PUT"],
   allowedHeaders: ["Content-Type", "Authorization", "token"],
   credentials: true,
