2b8659a3312954a104899fab389f03e1f5884f3c
SatyaKarthik05 <satyakarthikvelivela@gmail.com>
2025-03-28 17:35:59 +0530

    done

--- Screens/MainScreen.js
+++ Screens/MainScreen.js
@@ -57,7 +57,7 @@ const LoginScreen = () => {
         <View style={styles.buttonContainer}>
           <TouchableOpacity
             style={styles.button}
-            onPress={() => navigation.navigate("Starting Screen")}
+            onPress={() => navigation.navigate("CallCenterDashboard")}
           >
             <Text style={styles.buttonText}> Login </Text>
           </TouchableOpacity>
