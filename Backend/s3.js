ff0733064c78032c341e42f979f1b4edf33adb79
SatyaKarthik05 <satyakarthikvelivela@gmail.com>
2025-06-01 20:11:01 +0530

    for mowss

--- Backend/s3.js
+++ Backend/s3.js
@@ -1,10 +1,4 @@
-const AWS = require('aws-sdk');
-
-AWS.config.update({
-  accessKeyId: "AKIAWX2IFPZYF2O4O3FG", 
-  secretAccessKey: "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",
-  region: us-east-1, 
-});
+const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3();
 module.exports = s3;
